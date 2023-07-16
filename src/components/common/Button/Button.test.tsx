71d73097d85004dce6d5340cc8d30e63b983cee5
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2023-07-16 11:35:17 +0000

    .

--- src/components/common/Button/Button.test.tsx
+++ src/components/common/Button/Button.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Button from './Button'
-import { render } from '@testing-library/react';
+import { render,fireEvent } from '@testing-library/react';
 
 describe('Button', () => {
     test('renders Button component', () => {
         render(<Button variant='outlined' >Click Me</Button>)
+    })
+
+    // write test for clicking on the component:
+    test("click on the button", () => {
+        const handleClick = jest.fn();
+
+        const { getByText } = render(
+            <Button onClick={handleClick} variant='outlined'>
+                Click Me
+            </Button>
+        );
+
+        fireEvent.click(getByText('Click Me'));
+
+        expect(handleClick).toHaveBeenCalledTimes(1);
     })
 })
